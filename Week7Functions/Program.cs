63f18e19274c100e37b299c973acaab7a39c24d1
Anastassia Brd. <anastassia.berdnikova@gmail.com>
2023-10-26 21:14:39 +0300

    add week7 code

---
+++ Week7Functions/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Are we coming or leaving? (in/out):");
+string userCoice = Console.ReadLine();
+
+if (userCoice == "in")
+{ PrintHello(); }
+else
+{ PrintGoodBye(); }
+
+
+
+static void PrintHello() /// function / method
+{
+    Console.WriteLine("Hello, world!");
+}
+
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later, aligator.");
+}
